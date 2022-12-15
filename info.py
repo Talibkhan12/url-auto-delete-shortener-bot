6016ef392fdd929551dba8afc2de1b2bf8290d3f
Talibkhan12 <120482970+Talibkhan12@users.noreply.github.com>
2022-12-15 22:41:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'mdisk.pro')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'OmegaLinks.in')
 URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '04e8ee10b5f123456a640c8f33195abc')
 
      # Auto Delete For Group Message (Self Delete) #
