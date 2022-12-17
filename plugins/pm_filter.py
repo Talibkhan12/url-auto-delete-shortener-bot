bcdc16fd8ca49cd6ce455d0fe5489e086504ce21
Talibkhan12 <120482970+Talibkhan12@users.noreply.github.com>
2022-12-17 18:22:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -108,7 +108,7 @@ async def next_page(bot, query):
 
     btn.insert(0,
         [
-            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://youtu.be/vVdpPkVPhUs')
+            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://youtu.be/2DsYtgocbLc')
         ]
     )
 
@@ -682,7 +682,7 @@ async def auto_filter(client, msg, spoll=False):
 
     btn.insert(0,
         [
-            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://youtu.be/vVdpPkVPhUs')
+            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://youtu.be/2DsYtgocbLc')
         ]
     )
 
